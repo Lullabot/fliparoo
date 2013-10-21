9151279cb1ade5057210ff96c522607887e9b365
Jason Pamental <jason@hwdesignco.com>
2013-10-21 12:41:19 -0400

    lots more little theme updates

--- js/main.js
+++ js/main.js
@@ -35,6 +35,50 @@ $('.display li').fliparoo($('.queue li')); // plain vanilla
 
 }(document));
 
+$(function() {
+	$('.tablink').click(function(e) {
+		$('.tablink').removeClass('active');
+		$(this).addClass('active');
+		$('.tabs').removeClass('active');
+		if ($(this).hasClass('tab1')) {
+			$('#tab1').addClass('active');
+		} else if ($(this).hasClass('tab2')) {
+			$('#tab2').addClass('active');
+		} else if ($(this).hasClass('tab3')) {
+			$('#tab3').addClass('active');
+		}
+		e.preventDefault();
+	});
+
+	// Only add the toggle class if there are child pages
+	$('.nice-menu-down > li > a + ul').after('<span class="js-toggle-dropdown"><span class="icon icon-plus-sign"></span></span>');
+	$('.js-toggle-dropdown').click(function(e) {
+		e.preventDefault();
+		$(this).parent().toggleClass('nav-dropdown-expanded');
+		$(this).find('span').toggleClass('icon-plus-sign icon-remove-sign');
+	});
+	if(Modernizr.mq('only all and (min-width: 900px)')) {
+		$('body').addClass('mq-desktop');
+	} else if(Modernizr.mq('only all and (min-width: 700px)')) {
+		$('body').addClass('mq-lg_tab');
+	} else if(Modernizr.mq('only all and (min-width: 400px)')) {
+		$('body').addClass('mq-phone_wide-sm_tab');
+	} else {
+		$('body').addClass('mq-phone');
+	}
+
+
+			$('#nav-toggle').click(function(e) {
+				e.preventDefault();
+				$('.main-menu').toggleClass('open');
+				$('body').toggleClass('nav-open');
+				$(this).find('span').toggleClass('icon-reorder icon-remove-sign');
+
+			});
+
+
+});
+
 
 
 /*! Normalized address bar hiding for iOS & Android (c) @scottjehl MIT License */
